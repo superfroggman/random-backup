6a4f5878131a79b33e5e31f640c10c607ef98355
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-12-07 17:19:24 +0100

    lists selected directory

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const fs = require("fs");
+var path = require("path");
 
-console.log("hello world");
+var args = process.argv.slice(2);
+
+console.log(args);
+
+var location = args[0];
+
+const dirs = fs.readdirSync(location);
+
+dirs.forEach(dir => {
+    console.log(location + "/" + dir)
+});
+
